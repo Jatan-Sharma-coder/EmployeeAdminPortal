b335d2dc0253e58d4016054dec28cad3c3e5cbe8
Jatan-Sharma-Code <JatanSharma475@gmail.com>
2026-03-09 17:12:41 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entity;
+
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public ApplicationDbContext dbContext;
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext; 
+        }
+
+        [HttpPost("CreateNewEmployee")]
+        public IActionResult CreateNewEmployee(EmployeeDTO employee)
+        {
+            var newEmployee = new Employee
+            {
+                Name = employee.Name,
+                Email = employee.Email,
+                Phone = employee.Phone,
+                Salary = employee.Salary
+            };
+    
+            dbContext.Employees.Add(newEmployee);
+            dbContext.SaveChanges();
+    
+            return Ok(newEmployee);
+        }
+
+        [HttpGet("GetEmployeeById")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            return Ok(dbContext.Employees.Find(id));
+        }
+
+        [HttpGet("GetEmployeesList")]
+        public IActionResult GetEmployeesList()
+        {
+            return Ok(dbContext.Employees.ToList());
+        }
+
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeAdminPortal.Models.Entity;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
